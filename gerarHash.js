59b590f747acc1919b6255e9b576b95cc2774c43
Seu Nome <paulo.opensource@gmail.com>
2025-06-16 00:00:14 -0300

    Alteração no arquivo gerarHash.js

--- gerarHash.js
+++ gerarHash.js
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
-// gerarHash.js
-const bcrypt = require('bcrypt');
 
-const novaSenha = '123456'; // 🛠️ Substitua pela nova senha desejada
+// gerarHash.js
+const bcrypt = require('bcryptjs');
+
+const novaSenha = '102030'; // 🛠️ Substitua pela nova senha desejada
 
 bcrypt.hash(novaSenha, 10, (err, hash) => {
   if (err) {
@@ -11,17 +11,5 @@ bcrypt.hash(novaSenha, 10, (err, hash) => {
     console.log('Hash gerado:', hash);
   }
 });
-=======
-// gerarHash.js
-const bcrypt = require('bcrypt');
 
-const novaSenha = '123456'; // 🛠️ Substitua pela nova senha desejada
 
-bcrypt.hash(novaSenha, 10, (err, hash) => {
-  if (err) {
-    console.error('Erro ao gerar hash:', err);
-  } else {
-    console.log('Hash gerado:', hash);
-  }
-});
->>>>>>> 6fa9314911287a35326b119a0567039e1d8357f9
